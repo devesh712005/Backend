3b21aa0e5750c775ab91661c7d56efa7b2ba2818
devesh712005 <deveshsingh712005@gmail.com>
2026-01-03 21:56:59 +0530

    Feat: Added if cachedData not found in redis it will cached from local

--- redis-playground2/server/api/products.js
+++ redis-playground2/server/api/products.js
@@ -2,13 +2,11 @@ export const getProducts = () =>
   new Promise((resolve, reject) => {
     setTimeout(() => {
       resolve({
-        products: [
-          {
-            id: 1,
-            name: "Product1",
-            price: 100,
-          },
-        ],
+        products: {
+          id: 1,
+          name: "Product2",
+          price: 100,
+        },
       });
     }, 2000);
   });
--- redis-playground2/server/app.js
+++ redis-playground2/server/app.js
@@ -16,12 +16,17 @@ app.get("/", (req, res) => {
   res.send("Hello from express");
 });
 app.get("/products", async (req, res) => {
-  const cachedData = await redis.get("age");
-  console.log(cachedData);
+  const cachedData = await redis.get("products");
   if (cachedData) {
     console.log("Get from cached");
     return res.json({ message: JSON.parse(cachedData) });
   }
+  console.log("cached missed loading from source data");
+  const data = await getProducts();
+  console.log(data);
+  console.log(data.products.name);
+  await redis.set("products", JSON.stringify(data.products.name), "EX", 60);
+  return res.json(data.products.name);
 });
 app.listen(3000, () => {
   console.log("Server is running on 3000");
